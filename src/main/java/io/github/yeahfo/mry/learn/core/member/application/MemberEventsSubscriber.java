f4aa9833ca229cf06273810692c6e952fe113b22
yeahfo <11086665@qq.com>
2024-07-20 16:51:35 +0800

    nothing todo

--- src/main/java/io/github/yeahfo/mry/learn/core/member/application/MemberEventsSubscriber.java
+++ src/main/java/io/github/yeahfo/mry/learn/core/member/application/MemberEventsSubscriber.java
@@ -5,6 +5,8 @@ import io.eventuate.tram.events.subscriber.DomainEventHandlers;
 import io.eventuate.tram.events.subscriber.DomainEventHandlersBuilder;
 import io.github.yeahfo.mry.learn.core.member.domain.event.MemberCreatedEvent;
 import io.github.yeahfo.mry.learn.core.member.domain.event.MemberDomainEvent;
+import io.github.yeahfo.mry.learn.core.tenant.domain.event.TenantDomainEvent;
+import io.github.yeahfo.mry.learn.core.tenant.domain.event.TenantPlanUpdatedEvent;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,7 +15,17 @@ public class MemberEventsSubscriber {
         return DomainEventHandlersBuilder
                 .forAggregateType( MemberDomainEvent.aggregateType )
                 .onEvent( MemberCreatedEvent.class, this::memberCreatedEventHandler )
+                .andForAggregateType( TenantDomainEvent.aggregateType )
+                .onEvent( TenantPlanUpdatedEvent.class, this::tenantPlanUpdatedEventHandler )
                 .build( );
+    }
+
+    private void tenantPlanUpdatedEventHandler( DomainEventEnvelope< TenantPlanUpdatedEvent > envelope ) {
+        System.err.println( "tenantPlanUpdatedEventHandler begin -> " );
+        System.err.println( envelope.getAggregateId( ) );
+        System.err.println( envelope.getAggregateType( ) );
+        System.err.println( envelope.getEvent( ).trigger( ) );
+        System.err.println( "<- tenantPlanUpdatedEventHandler end." );
     }
 
     private void memberCreatedEventHandler( DomainEventEnvelope< MemberCreatedEvent > envelope ) {
--- src/main/java/io/github/yeahfo/mry/learn/core/tenant/domain/event/TenantCreatedEvent.java
+++ src/main/java/io/github/yeahfo/mry/learn/core/tenant/domain/event/TenantCreatedEvent.java
@@ -2,7 +2,5 @@ package io.github.yeahfo.mry.learn.core.tenant.domain.event;
 
 import io.github.yeahfo.mry.learn.core.common.domain.User;
 
-import java.util.List;
-
 public record TenantCreatedEvent( User trigger ) implements TenantDomainEvent {
 }
