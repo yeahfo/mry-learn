b420dce3219e860a2b86052b9cce0f80d1b98904
yeahfo <11086665@qq.com>
2024-08-03 17:42:14 +0800

    nothing todo

--- src/main/java/io/eventuate/messaging/redis/spring/common/RedissonClients.java
+++ src/main/java/io/eventuate/messaging/redis/spring/common/RedissonClients.java
@@ -1,45 +1,16 @@
 package io.eventuate.messaging.redis.spring.common;
 
 import lombok.Getter;
-import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
-import org.redisson.config.Config;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
+@Getter
 public class RedissonClients {
-    private Logger logger = LoggerFactory.getLogger(getClass());
-
-    private RedisServers redisServers;
-    @Getter
-    private List<RedissonClient> redissonClients;
+    private final List< RedissonClient > redissonClients;
 
     public RedissonClients( RedissonClient redissonClient ) {
         this.redissonClients = Collections.singletonList( redissonClient );
-    }
-
-    public RedissonClients( RedisServers redisServers) {
-        this.redisServers = redisServers;
-
-        redissonClients = redisServers
-                .getHostsAndPorts()
-                .stream()
-                .map(this::createRedissonClient)
-                .collect(Collectors.toList());
-    }
-
-    private RedissonClient createRedissonClient(RedisServers.HostAndPort hostAndPort) {
-        logger.info("Creating redisson client");
-        Config config = new Config();
-        config.useSingleServer().setRetryAttempts(Integer.MAX_VALUE);
-        config.useSingleServer().setRetryInterval(100);
-        config.useSingleServer().setAddress(String.format("redis://%s:%s", hostAndPort.getHost(), hostAndPort.getPort()));
-        logger.info("Created redisson client");
-        RedissonClient redissonClient = Redisson.create(config);
-        return redissonClient;
     }
 }
