5fd69046c113723d8c6d1d5e2f1aea0d8c92fec6
fong <isfong@qq.com>
2024-08-04 01:10:15 +0800

    nothing

--- @dev/mongo-init.js
+++ @dev/mongo-init.js
@@ -1,1 +1,12 @@
+// noinspection JSUnresolvedReference
+
+let db = connect("mongodb://localhost/admin");
+
+db.createUser(
+    {
+        user: "mongouser",
+        pwd: "mongopw",
+        roles: [ { role: "readWrite", db: "local_db" } ]
+    }
+)
 rs.initiate({'_id': 'rs0', 'members': [{'_id': 0, 'host': '127.0.0.1:27017'}]});
