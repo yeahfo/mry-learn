f9d89c8656294cfcc62c39a4b90e1ec232bfbd21
yeahfo <11086665@qq.com>
2024-08-05 21:05:11 +0800

    nothing todo

--- ui/web/app/ui/invoices/edit-form.tsx
+++ ui/web/app/ui/invoices/edit-form.tsx
@@ -9,7 +9,8 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import {Button} from '@/app/ui/button';
-import {updateInvoice} from "@/app/lib/actions";
+import {updateInvoice, State} from "@/app/lib/actions";
+import {useActionState} from 'react';
 
 export default function EditInvoiceForm({
                                             invoice,
@@ -18,9 +19,12 @@ export default function EditInvoiceForm({
     invoice: InvoiceForm;
     customers: CustomerField[];
 }) {
+    const initialState: State = {message: null, errors: {}};
     const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+    const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
+    console.log(state)
     return (
-        <form action={updateInvoiceWithId}>
+        <form action={formAction}>
             <div className="rounded-md bg-gray-50 p-4 md:p-6">
                 {/* Customer Name */}
                 <div className="mb-4">
@@ -115,7 +119,7 @@ export default function EditInvoiceForm({
             </div>
             <div className="mt-6 flex justify-end gap-4">
                 <Link
-                    href="/dashboard/invoices"
+                    href={`/dashboard/invoices`}
                     className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
                 >
                     Cancel
